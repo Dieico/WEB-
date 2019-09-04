3035bc946e67430ca4baa9c6ac091e4e21f1da3d
diego <diegolimd3@gmail.com>
2019-09-03 21:59:10 -0300

    .

---
+++ functions.js
@@ -0,0 +1,13 @@
+// function (){
+//   var valor = document.getElementById("firstName").value;
+//   if (valor == null || valor == ""|| /^\s+$/.test(valor) ) {
+//     window.alert("Preencha o campo de busca");
+//     return false;
+//   }
+//   return true;
+// }
+
+function (){
+  var nome = document.getElementById(firstName).value;
+  var senha = document.getElementById(firstName).value;
+}
